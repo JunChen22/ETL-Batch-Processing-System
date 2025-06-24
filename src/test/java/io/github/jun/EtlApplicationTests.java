7b28ae2cc1fc777f2fe773e844e09745b96ef22c
JunChen22 <ccjh522@gmail.com>
2025-06-24 19:26:06 -0400

    initial commit

---
+++ src/test/java/io/github/jun/EtlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
